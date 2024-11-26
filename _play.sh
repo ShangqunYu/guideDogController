a6303c6c5835763fdd11763e1faa91def725c08c
dmtrung <trungdangminh14012004@gmail.com>
2024-11-25 23:05:12 -0500

    rough terrain

--- _play.sh
+++ _play.sh
@@ -1,3 +1,3 @@
-# ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py --task Isaac-Velocity-Rough-Unitree-Go1-Play-v0
+./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py --task Isaac-Velocity-Rough-Unitree-Go1-Play-v0
 
-./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py --task Isaac-Velocity-Flat-Unitree-Go1-Play-v0
+# ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py --task Isaac-Velocity-Flat-Unitree-Go1-Play-v0
